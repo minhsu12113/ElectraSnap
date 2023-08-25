29451c9ceb47f55db88435c2021c65bbcbe4d223
Minh Su <minhsu12113@gmai.com>
2023-08-25 11:25:57 +0700

    done main views

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path') 
+const screenshot = require('desktop-screenshot')
 
 let mainWindow;
+let historyWindow;
+let snapWindow;
+
 const createWindow = () => {
     mainWindow = new BrowserWindow({
       width: 478,
@@ -23,6 +27,11 @@ app.whenReady().then(() => {
     createWindow()
 });
 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+})
+
+
 ipcMain.on('mimizedWindow', () => {
     BrowserWindow.getFocusedWindow().minimize();
 });
@@ -31,6 +40,56 @@ ipcMain.on('appExit', () => {
     app.quit();
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+function electSnap(){
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            screenshot(path.join(__dirname,'screenshot.png'), function(error, complete) {
+                if(error)
+                console.log("Screenshot failed", error);
+                else
+                    {                        
+                        imgpath = path.join(__dirname,'screenshot.png');
+                        resolve(imgpath)
+                    }
+            })
+        },250);
+      });  
+}
+
+ipcMain.handle('snapshot', async () => {
+    mainWindow.hide();
+    let path;
+    path = await electSnap();
+    return path;
+});
+
+ipcMain.on('open-snapshot-window', (event, imgpath) => {
+    snapWindow = new BrowserWindow(
+    {
+        webPreferences: {
+        preload: path.join(__dirname,'preload.js')
+      },
+        fullscreen: true,
+        frame: false,
+        resizable: false,
+        autoHideMenuBar: true,
+        icon: path.join(__dirname,'assets', 'elec-icon.ico')
+    })    
+
+    snapWindow.loadFile('./src/views/snapshot-window.html');
+    snapWindow.webContents.send('show-img', imgpath)
+    //snapWindow.openDevTools();
+    //mainWindow.s
 })
+
+ipcMain.on('openWindowHis', () => {
+    historyWindow = new BrowserWindow({
+        webPreferences: {
+          preload: path.join(__dirname,'preload.js')
+        },        
+        autoHideMenuBar: true,
+        icon: path.join(__dirname,'assets', 'elec-icon.ico'),
+    })
+    historyWindow.loadFile('./src/views/history-window.html');
+    mainWindow.hide();
+});
--- src/preload.js
+++ src/preload.js
@@ -3,4 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electronAPI', {
   mimizedWindow: () => ipcRenderer.send('mimizedWindow'),
   appExit: () => ipcRenderer.send('appExit'),
+  snapshot: () => ipcRenderer.invoke('snapshot'),
+  openSnapshotWindow: (imgPath) => ipcRenderer.send('open-snapshot-window', imgPath),
+  openWindowHis: () => ipcRenderer.send('openWindowHis'),
+  showImg: (callback) => ipcRenderer.on('show-img', callback)
 })
